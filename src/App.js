cb36b32cd7df7f09f556424003e500cc4df224d1
thatonedeveloperguy <Harikrishna3@>
2024-09-13 17:49:32 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import Log from "./pages/Login"
+import Register from './pages/Register';
+import { Route, Routes } from 'react-router-dom';
+import Display from './pages/Display';
+import User from './pages/User';
+import { useState } from 'react';
+import { createContext } from 'react';
+import Authorized from './pages/Authorized';
+export const GlobalC=createContext()
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ 
+  let a=10
+  let string="somerandom words"
+  
+const [theme,setTheme]=useState("black")
+
+function practiceroutes(){
+  return<>
+<GlobalC.Provider value={{setTheme,theme}}>
+<Routes>
+<Route path='/login'  element={<Log/>}   />
+<Route path='/register' element={<Register/>}    />
+<Route path='/' element={<Display/>} />
+<Route path='/user' element={<User/>} />
+<Route path='/user/:uid' element={<User/>} />
+<Route path='auth' element={<Authorized/>} />
+</Routes>
+</GlobalC.Provider>
+  </>
+
 }
+
+
+
+let obj={}
+
+  return <>
+{practiceroutes()}
+
+
+
+  </>
+}
+
+
+
+
 
 export default App;
